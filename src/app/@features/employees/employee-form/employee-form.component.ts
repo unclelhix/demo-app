64fe96689eb5b978602b0c2e1e98961f21669c75
Christian John Felix <christianfelix@getzclinical.com>
2022-09-21 22:31:43 +0800

    refactored

--- src/app/@features/employees/employee-form/employee-form.component.ts
+++ src/app/@features/employees/employee-form/employee-form.component.ts
@@ -4,9 +4,9 @@ import { DepartmentGroup } from '@features/departments/shared/models/department-
 import { Department } from '@features/departments/shared/models/department.model';
 import { DepartmentDropdownService } from '@features/departments/shared/services/department-dropdown.service';
 import { DepartmentService } from '@features/departments/shared/services/department.service';
-import { DropdownItems } from '@shared/components';
+
 import { DropDown } from '@shared/models/dropdown.model';
-import { map, Observable } from 'rxjs';
+import { debounceTime, map, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-employee-form',
@@ -15,35 +15,63 @@ import { map, Observable } from 'rxjs';
 })
 export class EmployeeFormComponent implements OnInit {
   frmEmloyee:FormGroup = {} as FormGroup;
-  department: Department = {} as Department;
-  departmentGroup: DepartmentGroup = {} as DepartmentGroup;
 
   departments$ = new Observable<Department[]>;
   departmentGroup$ =  new Observable<DepartmentGroup[]>;
 
-  departmentModel: DropDown = {
-    label: 'Department'
-  }
-  departmentGroupModel: DropDown = {
-    label: 'Department Group'
-  }
+  departmentDp:DropDown[] = [];
+  departmentGroupDp:DropDown[] = [];
+
+  departmentId: number = 0;
+  departmentGroupId: number = 0;
+
   constructor(private fb: FormBuilder,
     private departmentService: DepartmentService,
     private departmentGroupService: DepartmentDropdownService) { }
 
   ngOnInit() {
+
     this.departments$ = this.departmentService
-    .getAllDepartment()
-    .pipe(map(res=>res.data));
-    this.frmEmloyee = this.fb.group({});
+                        .getAllDepartment()
+                        .pipe(map(res=>res.data));
+
+    this.departments$.subscribe((result)=>{
+
+      this.departmentDp = result.map(res=>({
+        label:res.description,
+        value: res
+      }));
+
+    });
+
+    this.frmEmloyee = this.fb.group({
+
+    });
+
   }
+
   departmentEventHanlder(value: any){
+
     console.log(value)
-    this.department = value;
+
     this.departmentGroup$ = this.departmentGroupService
     .getByDepartmentId(Number(value.id)).pipe(map(res => res.data));
+
+    this.departmentGroupDp = [];
+
+    this.departmentGroup$.subscribe((result)=>{
+      console.log(result)
+      this.departmentGroupDp = result.map(res=>({
+        label:res.description,
+        value: res
+      }));
+
+    });
+
   }
+
   departmentGroupEventHanlder(value: any){
-    this.departmentGroup = value;
+    this.departmentGroupId = value;
   }
+
 }
--- src/app/@shared/components/dropdown/dropdown.component.ts
+++ src/app/@shared/components/dropdown/dropdown.component.ts
@@ -1,9 +1,8 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { DropDown } from '@shared/models/dropdown.model';
 import { Observable } from 'rxjs';
-export interface DropdownItems {
-  items?: any;
-}
+
+
 @Component({
   selector: 'app-dropdown',
   templateUrl: './dropdown.component.html',
@@ -11,9 +10,11 @@ export interface DropdownItems {
 })
 export class DropdownComponent implements OnInit {
 
-  item$: any = {} as any;
-  @Input() items = new Observable<any>;
-  @Input() dropdownModel = {} as DropDown;
+  item: DropDown = {} as DropDown;
+  @Input() items: DropDown[] = [];
+  @Input() labelFor: string = '';
+  @Input() labelText: string = '';
+  // @Input() dropdownModel = {} as DropDown;
   @Output() itemValueChange = new EventEmitter<any>();
 
   constructor() { }
